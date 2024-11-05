5e1010667f45acd7eccc4e0d1b6b118efd276e67
Aleksei Lazunin <gegcuk@gmail.com>
2024-11-05 22:34:35 +0000

    feat: Add User, Role, and Address entities with role-based permissions
    - Introduced `User` entity to represent application users, including fields for `username`, `email`, `password`, and role associations.
    - Added `Role` entity to manage roles with `AppRole` enum defining role types (USER, SELLER, ADMIN).
    - Created `Address` entity with `addressId` for future address management.
    - Established `@ManyToMany` relationship between `User` and `Role` entities for flexible role assignments.

---
+++ src/main/java/uk/gegc/ecommerce/sbecom/model/Role.java
@@ -0,0 +1,27 @@
+package uk.gegc.ecommerce.sbecom.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int roleID;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, name = "role_name")
+    private AppRole roleName;
+
+    public Role(AppRole roleName) {
+        this.roleName = roleName;
+    }
+}
