065c0153ddd6b25abc4ac26ce9981a3829bc91b3
alekseiLazunin <gegcuk@gmail.com>
2024-11-13 22:03:59 +0000

    feat: Implement JWT-based authentication
    - Added `AuthEntryPointJwt` to handle unauthorized access responses with structured JSON output.
    - Created `AuthTokenFilter` to filter requests, validate JWT tokens, and set user authentication in the security context.
    - Implemented `JwtUtils` for JWT token operations, including generation, validation, and extraction of username from token.
    - Added `LoginRequest` and `LoginResponse` classes to handle login payload and structured response.

---
+++ src/main/java/uk/gegc/ecommerce/sbecom/security/jwt/LoginRequest.java
@@ -0,0 +1,11 @@
+package uk.gegc.ecommerce.sbecom.security.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    private String username;
+    private String password;
+}
