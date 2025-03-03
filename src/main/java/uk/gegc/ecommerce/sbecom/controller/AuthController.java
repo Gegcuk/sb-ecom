17bd4c8d11ed2f2e2e8970e75173fa5a426e32a9
alekseiLazunin <gegcuk@gmail.com>
2025-03-03 23:44:45 +0000

    feat(auth): Add endpoints for fetching user details and logout functionality
    ### Changes:
    - **Added `/username` endpoint** (`GET`):
      - Retrieves the currently authenticated user's details.
      - Returns `UserInfoResponse` containing user ID, username, and roles.
      - Housekeeping
    
    - **Added `/logout` endpoint** (`POST`):
      - Clears JWT authentication cookie on logout.
      - Returns a response confirming successful sign-out.
    
    ### Impact:
    - Enables frontend or API consumers to retrieve the current user's identity and roles.
    - Implements secure logout functionality by invalidating authentication cookies.

--- src/main/java/uk/gegc/ecommerce/sbecom/controller/AuthController.java
+++ src/main/java/uk/gegc/ecommerce/sbecom/controller/AuthController.java
@@ -2,7 +2,9 @@ package uk.gegc.ecommerce.sbecom.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,11 +13,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uk.gegc.ecommerce.sbecom.dto.request.SignupRequest;
 import uk.gegc.ecommerce.sbecom.repository.RoleRepository;
 import uk.gegc.ecommerce.sbecom.security.response.MessageResponse;
@@ -65,15 +63,15 @@ public class AuthController {
 
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
 
-        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
 
         List<String> roles = userDetails.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .toList();
 
-        UserInfoResponse response = new UserInfoResponse(userDetails.getId(), userDetails.getUsername(), roles, jwtToken );
+        UserInfoResponse response = new UserInfoResponse(userDetails.getId(), userDetails.getUsername(), roles );
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString()).body(response);
 
     }
 
@@ -121,6 +119,25 @@ public class AuthController {
         userRepository.save(user);
 
         return ResponseEntity.ok(new MessageResponse("User registered successfully"));
+    }
 
+    @GetMapping("/username")
+    public ResponseEntity<?> currentUserDetails(Authentication authentication){
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        UserInfoResponse response = new UserInfoResponse(userDetails.getId(), userDetails.getUsername(),
+                roles);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(){
+        ResponseCookie cookie = jwtUtils.getCleanCookie();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body(new MessageResponse("You have been signed out"));
     }
 }
